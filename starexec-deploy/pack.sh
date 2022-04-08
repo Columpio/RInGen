d4c52694f8237f7d6352e4b7d17b1ee3d2f4a5e9
Yurii Kostyukov <kostyukov.yurii@gmail.com>
2022-04-08 17:15:16 +0300

    [fix] Fixed StarExec deploy

--- starexec-deploy/pack.sh
+++ starexec-deploy/pack.sh
@@ -7,13 +7,14 @@ elif [[ ! -f "vampire" ]]; then
     exit 1
 fi
 
-rm -rf publish
+rm -rf publish tmp
 rm -f solver.zip
+mkdir tmp
 
 RINGEN_BASE_DIR="$(dirname "$(pwd)")"
 dotnet publish -c Release -r rhel.7-x64 -p:PublishReadyToRun=true "$RINGEN_BASE_DIR/RInGen.sln"
 
 cp -r "$RINGEN_BASE_DIR/bin/Release/net6.0/rhel.7-x64/publish/" .
 cp cvc vampire publish
-zip -r solver.zip binaries publish starexec_description.txt
+zip -r solver.zip bin publish starexec_description.txt
 set +x
